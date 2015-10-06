444a14b2be9fc8fb6f2a5a8ab7d034cf9f02d187
Alex Torres <jalexandert@gmail.com>
2015-10-06 14:07:12 -0500

    Step 3.

--- autolayout/autolayout/ViewController.swift
+++ autolayout/autolayout/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topRedView: UIView!
+    @IBOutlet weak var topYellowView: UIView!
+    @IBOutlet weak var bottomView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,48 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillLayoutSubviews() {
+        
+        if UIDevice.currentDevice().orientation == UIDeviceOrientation.LandscapeLeft || UIDevice.currentDevice().orientation == UIDeviceOrientation.LandscapeRight {
+            
+            var rect = topRedView.frame
+            rect.size.width = 254
+            rect.size.height = 130
+            topRedView.frame = rect
+            
+            rect = topYellowView.frame
+            rect.origin.x = 294
+            rect.size.width = 254
+            rect.size.height = 130
+            topYellowView.frame = rect
+            
+            rect = bottomView.frame
+            rect.origin.y = 170
+            rect.size.width = 528
+            rect.size.height = 130
+            bottomView.frame = rect
+            
+        } else {
+            
+            var rect = topRedView.frame
+            rect.size.width = 130
+            rect.size.height = 254
+            topRedView.frame = rect
+            
+            rect = topYellowView.frame
+            rect.origin.x = 170
+            rect.size.width = 130
+            rect.size.height = 254
+            topYellowView.frame = rect
+            
+            rect = bottomView.frame
+            rect.origin.y = 295
+            rect.size.width = 280
+            rect.size.height = 254
+            bottomView.frame = rect
+        }
     }
 
 
